44c48478befbbe3251c2501aa394e6407959ee6d
Dylan DeRouchie <dylan.f.derouchie@gmail.com>
2020-04-15 14:16:14 -0500

    seeded roles into DB as well as done my first migration

--- Physis/Data/ApplicationDbContext.cs
+++ Physis/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +12,38 @@ namespace Physis.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(
+            new IdentityRole 
+            { 
+                Id = "6793de18-a3fb-4d2e-bcdd-2b4568f9d936",
+                ConcurrencyStamp = "f24c7eeb-fd57-47d2-9d34-e746b1801b7e",
+                Name = "Tree Planter", 
+                NormalizedName = "TREE PLANTER" 
+            });
+
+            builder.Entity<IdentityRole>().HasData(
+            new IdentityRole
+            {
+                Id = "eafe5073-a58d-48e0-9cad-0380ec8ac8cd",
+                ConcurrencyStamp = "f2204c01-202b-49fd-baaf-3129b52c54c6",
+                Name = "Vendor",
+                NormalizedName = "VENDOR"
+            });
+
+            builder.Entity<IdentityRole>().HasData(
+            new IdentityRole
+            {
+                Id = "872c4215-ba53-4399-9b3a-ccf186d11b17",
+                ConcurrencyStamp = "43e75581-d2b4-4f5d-b5dc-201673334a5d",
+                Name = "Donator",
+                NormalizedName = "DONATOR"
+            });
         }
     }
 }
---
+++ Physis/Data/Migrations/20200415190843_Initial.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Physis.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "6793de18-a3fb-4d2e-bcdd-2b4568f9d936", "f24c7eeb-fd57-47d2-9d34-e746b1801b7e", "Tree Planter", "TREE PLANTER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "eafe5073-a58d-48e0-9cad-0380ec8ac8cd", "f2204c01-202b-49fd-baaf-3129b52c54c6", "Vendor", "VENDOR" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "872c4215-ba53-4399-9b3a-ccf186d11b17", "43e75581-d2b4-4f5d-b5dc-201673334a5d", "Donator", "DONATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6793de18-a3fb-4d2e-bcdd-2b4568f9d936");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "872c4215-ba53-4399-9b3a-ccf186d11b17");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "eafe5073-a58d-48e0-9cad-0380ec8ac8cd");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
